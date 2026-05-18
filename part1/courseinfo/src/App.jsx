3104d9f7b5059ee917cb5ee26ea72a9d5ec649f3
turboaux <jje.uribe@gmail.com>
2026-05-17 18:28:35 -0600

    part1: Courseinfo app

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+function Header({ title }) {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+function Content({ parts }) {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises}/>
+      <Part name={parts[1].name} exercises={parts[1].exercises}/>
+      <Part name={parts[2].name} exercises={parts[2].exercises}/>
+    </div>
+  )
+}
+
+function Total({ parts }) {
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
+}
+
+function Part({ name, exercises }) {
+  return (
+    <p>{name} {exercises}</p>
+  )
+}
+
+function App() {
+  const course = {  
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      }, 
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  } 
+
+  return (
+    <div>
+      <Header title={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
